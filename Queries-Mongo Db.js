9ad78db4b1438ebf893dc4f6a037864d146125c2
Mirroy25 <112901601+Mirroy25@users.noreply.github.com>
2023-07-02 11:12:21 -0500

    Update Queries-Mongo Db.js

--- Queries-Mongo Db.js
+++ Queries-Mongo Db.js
@@ -22,3 +22,61 @@ db.CitaMedica.distinct('Medico.Persona.NombreMedico', {HoraProgramada:'11:00'})
 
 /* Indicar los distintos Horas Programadas*/
 db.CitaMedica.distinct('HoraProgramada')
+
+/* buscar cita medica por usuario y lanzar solo el medico centro de salud y fecha programada*/
+db.CitaMedica.find(
+    {
+        "Usuario": {
+            "NombreUsuario": "Patricia",
+            "ApellidoPaternoUsuario": "Rojas",
+            "ApellidoMaternoUsuario": "Soto",
+            "TipoDocumentoIdentificacionUsuario": "DNI",
+            "NumeroDocumentoIdentificacionUsuario": "70000015",
+            "FechaNacimientoUsuario": ISODate("1990-01-15")
+        }
+    },
+    {
+      "Medico": 1, "CentroSalud":1, "FechaProgramada":1, "_id": 0
+    }
+)
+/*busca el centro medico que satisfaga las condiciones dadas*/
+db.CentroSalud.find(
+    {
+             $and : [
+                {"NombreCentroSalud": "Clinica Bienestar"},
+                {"Direccion": "Calle Los Pinos 123"}
+             ]
+    }
+)
+/* Distintos Usuarios*/
+
+db.CitaMedica.distinct("Usuario.NombreUsuario")
+
+/* Mostrar la cantidad de medico por cada especialidad*/
+
+db.CitaMedica.aggregate([
+   {
+      $unwind: "$Medico"
+   },
+   {
+        $group:{
+          _id: "$Medico.Especialidad",
+          count:{
+            $sum:1
+          }
+        }
+    }
+])
+/*Saber si un medico esta en un centro de salud*/
+db.CentroSalud.findOne({ Medico: { $elemMatch: { nombre: "NombreMedico" } } })
+
+/*saber los medicos que hay por cada centro de salud*/
+
+db.CentroSalud.aggregate([
+    {
+        $group: {
+            _id: "$Persona.NombreMedico",
+            NumeroCentrosSalud: { $sum: 1 }
+      }
+    }
+])
